277a45571b5e406a279b3630e205a883c290dfd2
Rapheal <nhuxhr@gmail.com>
2025-09-11 23:30:51 +0100

    feat(lsp): add Rust Analyzer cfg

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
 require("config.lazy")
 
 vim.cmd("colorscheme kanagawa")
+
+local lspconfig = require("lspconfig")
+lspconfig.rust_analyzer.setup({
+	settings = {
+		["rust-analyzer"] = {
+			procMacro = {
+				enable = true,
+			},
+		},
+	},
+})
