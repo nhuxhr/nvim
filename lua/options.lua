997a1819bdd918728cf823e156c326a3ac7e5d9e
Rapheal <nhuxhr@gmail.com>
2026-04-24 23:51:58 +0000

    feat: enable `tabufline` and add `sessionoptions` cfg

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,5 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.opt.sessionoptions = blank, buffers, curdir, folds, help, tabpages, winsize, terminal, localoptions
