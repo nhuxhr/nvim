592e9dc0b6b2e520738fe3e7ae1842bd7db0e445
Rapheal <nhuxhr@gmail.com>
2026-04-24 23:52:57 +0000

    feat(mappings): add save shortcut and line movement keybindings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,4 +8,12 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("i", "kj", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Normal Mode: Move current line
+map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+
+-- Visual Mode: Move selected block
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
