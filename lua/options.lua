9ccb8aff6badc01679d00e82dd4b4c9bd38191fe
Rapheal <nhuxhr@gmail.com>
2026-04-25 00:19:34 +0000

    feat: add `localleader` key and editor formatting options

--- lua/options.lua
+++ lua/options.lua
@@ -5,4 +5,10 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
+vim.cmd "set relativenumber"
+vim.cmd "set expandtab"
+vim.cmd "set tabstop=2"
+vim.cmd "set softtabstop=2"
+vim.cmd "set shiftwidth=2"
+
 vim.opt.sessionoptions = blank, buffers, curdir, folds, help, tabpages, winsize, terminal, localoptions
