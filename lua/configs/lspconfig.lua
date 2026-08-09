be55cb9d3aa94c7cdf3f17ed6d3c36dd877e3047
Rapheal <nhuxhr@gmail.com>
2026-08-09 23:49:29 +0000

    feat(lsp): add `tailwindcss` server and ignore unknown CSS at-rules

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,17 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "rust_analyzer", "taplo", "ts_ls", "biome" }
+vim.lsp.config("cssls", {
+  settings = {
+    css = {
+      validate = true,
+      lint = {
+        unknownAtRules = "ignore", -- Tells cssls to ignore @variant and @theme
+      },
+    },
+  },
+})
+
+local servers = { "html", "cssls", "rust_analyzer", "taplo", "ts_ls", "biome", "tailwindcss" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
