fa9eac568a6e75e16982476da540e99f348486ca
Rapheal <nhuxhr@gmail.com>
2026-08-09 23:50:33 +0000

    feat(flutter): add Flutter tooling and `dcmls` LSP server

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -11,7 +11,7 @@ vim.lsp.config("cssls", {
   },
 })
 
-local servers = { "html", "cssls", "rust_analyzer", "taplo", "ts_ls", "biome", "tailwindcss" }
+local servers = { "html", "cssls", "rust_analyzer", "taplo", "ts_ls", "biome", "tailwindcss", "dcmls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
