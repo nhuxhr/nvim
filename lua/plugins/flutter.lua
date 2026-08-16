11f84f7bae2116450f64cf788b850526dccf15c9
Rapheal <nhuxhr@gmail.com>
2026-08-16 03:19:49 +0000

    feat(plugins): add leader keybindings for flutter commands

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -5,5 +5,12 @@ return {
     "nvim-lua/plenary.nvim",
     "stevearc/dressing.nvim", -- optional for vim.ui.select
   },
+  keys = {
+    { "<leader>dfs", "<cmd>FlutterRun<cr>", desc = "Flutter Run" },
+    { "<leader>dfr", "<cmd>FlutterReload<cr>", desc = "Flutter Hot Reload" },
+    { "<leader>dfR", "<cmd>FlutterRestart<cr>", desc = "Flutter Hot Restart" },
+    { "<leader>dfl", "<cmd>FlutterLogToggle<cr>", desc = "Toggle Flutter Log" },
+    { "<leader>dfq", "<cmd>FlutterQuit<cr>", desc = "Quit Flutter App" },
+  },
   config = true,
 }
