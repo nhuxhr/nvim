71d2e1e663fc87d7a7a11fb42b709b601e9b6591
Rapheal <nhuxhr@gmail.com>
2026-08-20 05:12:40 +0000

    feat(plugins): add `lorem.nvim` for placeholder text generation

---
+++ lua/plugins/lorem.lua
@@ -0,0 +1,9 @@
+return {
+  "derektata/lorem.nvim",
+  cmd = { "Lorem" },
+  opts = {
+    sentence_length = "mixed", -- using a default configuration
+    comma_chance = 0.3, -- 30% chance to insert a comma
+    max_commas = 2, -- maximum 2 commas per sentence
+  },
+}
